c73a44aa60c9d65e2f9644bd2ba400c799f7693b
Wili0710 <66407599+wili0710@users.noreply.github.com>
2021-01-19 19:40:16 +0700

    Click Counter with Unit Testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [showError, setShowError] = useState(false)
+
+  const counter = (todo) => {
+    if( todo === "increment" ){
+      if( count + 1 > 0 ){
+        setShowError(false)
+
+        setCount( count + 1 )
+
+      }else{
+        setCount( count + 1 )
+
+      }
+    }else{
+      if( count - 1 < 0 ) {
+        setShowError(true)
+        
+        setCount( 0 )
+      }else{
+        setCount( count - 1 )
+        
+      }
+    }
+  }
+
   return (
     <div data-test="component-app">
-      <h1>
-        App
+      <h1 data-test="counter-display">
+        The counter is currently&nbsp;
+        <span data-test="count">
+          {count}
+        </span>
       </h1>
+      <button 
+        data-test="increment-button"
+        onClick={()=>counter("increment")}
+      >
+        Increment Counter
+      </button>
+      &nbsp;
+      <button
+        data-test="decrement-button"
+        onClick={()=>counter("decrement")}
+      >
+        Decrement Counter
+      </button>
+      {
+        showError?
+          <div 
+            data-test="show-error"
+          >
+            No Count Below Zero 
+          </div>
+        :
+          null
+      }
     </div>
   );
 }
